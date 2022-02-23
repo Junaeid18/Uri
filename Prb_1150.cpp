7b6eb71e7e56085b1f00f88371ec60fa0b64efa9
Junaeid18 <mdjunaeid98@gmail.com>
2022-02-24 00:00:28 +0600

    Prb_1150 solved

---
+++ Prb_1150.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x,z,start {0},sum = 0;
+    cin>>x;
+    cin>>z;
+    while (x >= z)
+    {
+        cin>> z;
+    }
+    sum += x;
+    while(sum < z){
+        sum += x;
+        ++x;
+        ++start;
+    }
+    cout<<start<<endl;
+    return 0;
+}
