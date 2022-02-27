a88afa4eb7a2d49708eee77dd9ccb063e0b75998
Junaeid18 <mdjunaeid98@gmail.com>
2022-02-27 16:54:15 +0600

    Prb_1142 updated

--- Prb_1142.cpp
+++ Prb_1142.cpp
@@ -8,16 +8,5 @@ int main()
         cout<<var<<" "<<var+1<<" "<<var+2<<" "<<"PUM"<<endl;
         var += 4;
     }
-
-
     return 0;
 }
-    /* int line,num {1},line1 {1};
-    cin>>line;
-    //line = (line * 3) + line;
-    while (line1 <= line )
-    {
-        cout<<num<<" "<<++num<<" "<<++num<<" "<<"PUM"<<endl;
-        num += 2;
-        ++line1;
-    } */
